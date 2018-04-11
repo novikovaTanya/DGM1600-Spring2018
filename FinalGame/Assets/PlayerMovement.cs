0a57df28cd8de7d64a28474e8a4f013bc0c3530f
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-04-11 11:32:01 -0600

    Add basic shapes

---
+++ FinalGame/Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float forwardForce = 100f;
+
+	void FixedUpdate () {
+		rb.AddForce (forwardForce * Time.deltaTime, 0, 0);
+
+		if (Input.GetButton ("Jump"))
+			rb.AddForce (2000 * Time.deltaTime, 0, 0);
+	}
+}
