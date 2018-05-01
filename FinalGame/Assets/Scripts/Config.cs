4fdf88ae2bc15c6beee5a65ec32d9916a23c44cd
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-05-01 15:31:34 -0600

    Add Character Controller and Manager

---
+++ FinalGame/Assets/Scripts/Config.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class Config : ScriptableObject {
+	
+	public float speed = 10f;
+	public float jumpSpeed = 10f;
+	public float gravity = 20f;
+
+}
--- FinalGame/Assets/Scripts/PlayerMovement.cs
+++ FinalGame/Assets/Scripts/PlayerMovement.cs
@@ -5,30 +5,18 @@ using UnityEngine;
 [CreateAssetMenu]
 public class PlayerMovement : ScriptableObject {
 
-//	public Rigidbody rb;
-	public float speed = 1f;
+	public Config config;
 	public Vector3 direction = Vector3.zero;
 
-	void FixedUpdate () {
-//		rb.AddForce (10 * Time.deltaTime, 0, 0);
-
-//		if (Input.GetButton ("Jump"))
-//			rb.AddForce (2000 * Time.deltaTime, -1000 * Time.deltaTime, 0);
-
-
-
-	}
-
 	public void Move(CharacterController controller, Transform transform) {
-//		if (controller.isGrounded) {
-//			direction.x = inputX.SetInput() * config.speed;
-//			direction.z = inputY.SetInput() * config.speed;
-//			direction.y = (Input.GetButton("Jump")) ? config.jumpSpeed : 0;
-//
-//			direction = transform.TransformDirection(direction);
-//		}
-		direction.x = 1 * speed;
-		direction.y -= 0.1f * Time.deltaTime;
+		if (controller.isGrounded) {
+			Debug.Log (Input.GetButton("Jump"));
+			direction.x = Input.GetAxis("Horizontal") * config.speed;
+			direction.y = (Input.GetButton("Jump")) ? config.jumpSpeed : 0;
+
+			direction = transform.TransformDirection(direction);
+		}
+		direction.y -= config.gravity * Time.deltaTime;
 		controller.Move(direction * Time.deltaTime);
 	}
 }
