69518172271b58d86a837382842936cf25ece173
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-05-02 21:39:42 -0600

    Create Menu with Options

---
+++ FinalGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Menu : MonoBehaviour {
+	public enum Nicknames {SuperHero, SuperEgg, StinkyEgg, Weirdo, Player1};
+	public Nicknames nickname = new Nicknames ();
+
+	void Start () {
+		switch ((int)nickname) {
+		case 0:
+			Debug.Log ("");
+			break;
+		default:
+			Debug.Log ("ENUM DEFAULT");
+			break;
+		}
+	}
+	
+	void Update () {
+		
+	}
+}
