b06cef0e7397a82699e190692ffc89ada698dd16
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-05-03 08:13:54 -0600

    Add array, foreach and for statements

--- FinalGame/Assets/Scripts/CameraController.cs
+++ FinalGame/Assets/Scripts/CameraController.cs
@@ -6,7 +6,21 @@ public class CameraController : MonoBehaviour {
 	public Vector3 offset;
 
 
+
 	void LateUpdate () {
 		transform.position = player.position + offset;
+	}
+
+	public void Start() {
+		GameObject[] objects = (GameObject[]) Object.FindObjectsOfType(typeof(GameObject));
+
+		for (int i = 0; i < objects.Length; i++)
+			Debug.Log (objects[i]);
+
+		foreach (GameObject obj in objects)
+		{
+			Debug.Log("Name " + obj.name + " , Tag: " + obj.tag);
+		}
+			
 	}
 }
